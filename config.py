bdf55c887b046984620973985d0a1bb747dbc2dd
rasppi <bertpi@outlook.com>
2026-06-07 10:09:54 +0100

    add config

---
+++ config.py
@@ -0,0 +1,19 @@
+ODDSPAPI_KEY = "bfb812bb-86bd-46dc-b5ad-e53bdf40c775"
+
+BELGIAN_BOOKMAKERS = [
+    "unibet",
+    "bwin",
+    "ladbrokes",
+    #"betfirst",
+    #"circus",
+    #"jackbit",
+    #"vave",
+    #"betway",
+    "betfirst"
+]
+
+STARTING_BANKROLL = 1000
+
+EDGE_THRESHOLD = 0.03
+
+KELLY_FRACTION = 0.25
