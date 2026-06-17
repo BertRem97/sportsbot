18501cc50b3655a9fddfbbcecb59ccdf0e8ed44b
DonRemon97 <bert.remon@outlook.com>
2026-06-17 13:44:13 +0200

    edit apiwrapper

--- apiwrapper.py
+++ apiwrapper.py
@@ -2,37 +2,62 @@ import requests
 from config import ODDSPAPI_KEY
 import time
 from pprint import pprint
+from collections import defaultdict
 
 
 BASE_URL = "https://api.oddspapi.io"
 LAST_REQUEST = 0
 
+def compare_bookmakers_for_fixture(fixture):
+
+    fixture_id = fixture["fixtureId"]
+
+    market_map = defaultdict(dict)
+
+    for bookie in BOOKMAKERS:
+
+        params = {
+            "fixtureId": fixture_id,
+            "bookmaker": bookie,
+            "language": "en",
+            "verbosity": 3,
+            "apiKey": ODDSPAPI_KEY
+        }
+
+        try:
+            event = api_get("/v4/odds", params)
+
+            markets = event["bookmakerOdds"][bookie]["markets"]
+
+            for market_id, market in markets.items():
+
+                market_map[market_id][bookie] = market
+
+        except Exception as e:
+            print(f"{bookie}: {e}")
+
+    return market_map
+
+
+
 def extract_market_odds(fixture):
 
     tournament_id = fixture["fixtureId"]
-    params = {
-        "fixtureId": tournament_id,
-        "bookmakers": BOOKMAKER,
-        "language": "en",
-        "verbosity": 3,
-        "apiKey": ODDSPAPI_KEY
-    }
+    dict = defaultdict(list)
+    for bookie in BOOKMAKERS:
+        params = {
+            "fixtureId": tournament_id,
+            "bookmaker": bookie,
+            "language": "en",
+            "verbosity": 3,
+            "apiKey": ODDSPAPI_KEY
+        }
 
-    data = []
-    
-    event = api_get('/v4/odds', params)
-    bookmaker_odds = event["bookmakerOdds"]
-    data_bookie = bookmaker_odds[BOOKMAKER]
-    market = data_bookie["markets"]
-
-    for market_id, market_data in market.items():
-        outcomes = market_data["outcomes"]
-        market_data = outcomes[market_id]["players"]["0"]
-        data.append(market_data)
-
-    pprint(event)
-    pprint(event.keys())
-    return event, data
+        event = api_get('/v4/odds', params)
+        
+        dict[bookie].append(event) 
+     
+    return dict
 
 def api_get(endpoint, params):
 
@@ -135,7 +160,7 @@ def get_available_tournaments(
             continue
     return available
 
-BOOKMAKER = "bwin.be"
+BOOKMAKERS =  ["bwin.be", "unibet.be", "betano", "pinnacle", "bc.game"]
 tournaments = get_tournaments()[:20]
 
 print(
@@ -144,7 +169,7 @@ print(
 
 available = get_available_tournaments(
     tournaments,
-    BOOKMAKER
+    BOOKMAKERS[0]
 )
 
 print("\nBeschikbare competities:\n")
@@ -161,34 +186,26 @@ league_id = int(
 
 fixtures = get_odds_by_tournaments(
     [league_id],
-    BOOKMAKER)
+    BOOKMAKERS[0])
 
 print(f"\n{len(fixtures)} wedstrijden gevonden")
 
 
-for fixture in fixtures[:10]:
+for fixture in fixtures[:5]:
+    print(
+        f"\n{fixture['participant1Name']} - {fixture['participant2Name']}")
 
-    event, data = extract_market_odds(fixture)
-    name_team_1 = event['participant1Name']
-    name_team_2 = event['participant2Name']
-    sport = event['sportName']
-    statusname = event['statusName']
-    tournament_name = event['tournamentName']
-    print(f"{name_team_1} - {name_team_2}")
-    print(sport)
-    print(statusname)
-    print(tournament_name)
-    print('-----------------------------------------------------------')
+    market_map = compare_bookmakers_for_fixture(fixture)
+    for market_id, bookmakers in market_map.items():
+        if len(list(bookmakers)) == len(BOOKMAKERS):
+            print(f"\nMARKET {market_id}")
+            for bookie, market in bookmakers.items():
+                print(f"  {bookie}")
 
-    for odd in data:
-        betslip_url = odd["betslip"]
-        last_change = odd["changedAt"]
-        limit = odd["limit"]
-        price = odd["price"]
-        print(betslip_url)
-        print(last_change)
-        print(limit)
-        print(price)
-        print('---------------------------')
-        break
-        
+                for outcome_id, outcome in market["outcomes"].items():
+                    player = outcome["players"]["0"]
+
+                    print(
+                        f"    outcome={outcome_id}"
+                        f" odds={player['price']}"
+                        f" betslip={player['betslip']}")
